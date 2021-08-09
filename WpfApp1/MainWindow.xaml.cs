16a7899248b12c1c061a223fb5f3f63bd0d52955
drakula <sergk20017@yandex.ru>
2021-08-09 23:04:14 +0300

    отображается в текстбоксе

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -11,9 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
-
-
+using Microsoft.VisualBasic;
 
 
 namespace WpfApp1
@@ -29,44 +29,63 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
-
+       
         private void grid_Loaded(object sender, RoutedEventArgs e)
         {
-            List<MyTable> result = new List<MyTable>(3);
-            result.Add(new MyTable(1, "Майкл Джексон", "Thriller", 1982));
-            result.Add(new MyTable(2, "AC/DC", "Back in Black", 1980));
-            result.Add(new MyTable(3, "Bee Gees", "Saturday Night Fever", 1977));
-            result.Add(new MyTable(4, "Pink Floyd", "The Dark Side of the Moon", 1973));
+            List<MyTable> result = new List<MyTable>(6);
+            result.Add(new MyTable(1, 1234, 1235, "yes", "Optimix", 1971));
+            result.Add(new MyTable(2, 5454, 5455, "yes", "Alebado", 1971));
+            result.Add(new MyTable(3, 3268, 32369, "yes", "Drakula", 1971));
+            result.Add(new MyTable(4, 8763, 8764, "yes", "MoryakSPb", 1971));
             grid.ItemsSource = result;
         }
 
         //Получаем данные из таблицы
-       /* private void grid_MouseUp(object sender, MouseButtonEventArgs e)
+        private void grid_MouseUp(object sender, MouseButtonEventArgs e)
         {
+            
             MyTable path = grid.SelectedItem as MyTable;
-            MessageBox.Show(" ID: " + path.Id1 + "\n Исполнитель: " + path.Vocalist + "\n Альбом: " + path.Album
-                + "\n Год: " + path.Year);
+            int parse = grid.SelectedIndex;
+            DataRowView rowView = grid.SelectedValue as DataRowView;
+            
+            if (parse==0)
+            {
+                string str = "Convert.ToString(parse)";
+                tb.Text = str;
+            }
+            else
+            {
+                string str ="  ";
+                tb.Text = str;
+            }
         }
-       */
+       
         private void add_Click(object sender, RoutedEventArgs e)
         {
-
+           
         }
 
         
     }
     class MyTable
     {
-        public MyTable(int Название, string Vocalist, string Album, int Year)
+        public MyTable(int name, int current_version, int final_version, string Upload_prototypes, string author,
+            int modified_date)
         {
-            this.Название = Название;
-            this.Vocalist = Vocalist;
-            this.Album = Album;
-            this.Year = Year;
+            this.name = name;
+            this.current_version = current_version;
+            this.final_version = final_version;
+            this.Upload_prototypes = Upload_prototypes;
+            this.author = author;
+            this.modified_date = modified_date;
         }
-        public int Название { get; set; }
-        public string Vocalist { get; set; }
-        public string Album { get; set; }
-        public int Year { get; set; }
+
+        public int name { get; set; }
+        public int current_version { get; set; }
+        public int final_version { get; set; }
+        public string Upload_prototypes { get; set; }
+        public string author { get; set; }
+        public int modified_date { get; set; }
+
     }
 }
